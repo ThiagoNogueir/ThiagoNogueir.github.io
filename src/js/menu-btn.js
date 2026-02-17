f7523d2a0bfd6c9345891bd9bea2fc4926636b37
Thiago Dos Santos Nogueira <thiagonogueira@fam.edu.br>
2026-02-16 21:41:37 -0300

    Ajuste responsividade

--- src/js/menu-btn.js
+++
@@ -1,7 +0,0 @@
-let menuToggle = document.querySelector('.toggle');
-let menu = document.querySelector('.menu');
-
-function toggler() {
-    menuToggle.classList.toggle('active');
-    menu.classList.toggle('active');
-}
